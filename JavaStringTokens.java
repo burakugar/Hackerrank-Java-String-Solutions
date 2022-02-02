e8362744505ee3312c38f1490e9e27be3ac63e71
Burak Ugar <50373019+lubimi@users.noreply.github.com>
2022-02-02 16:01:43 +0300

    Create JavaStringTokens.java

---
+++ JavaStringTokens.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+       if(s.trim().isEmpty()){
+   System.out.println("0");
+   System.out.println(s);
+} else {
+   String[] splitString = (s.replaceAll("^\\W+", "").split("[\\s!,?._'@]+"));
+   System.out.println(splitString.length);
+   for(String str: splitString) {
+        System.out.println(str);
+   }
+}
+}
+}
+
